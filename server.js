73f633139bc24fe04588afb566a568375e71ef3a
whiskyplausible <bumble@lagosta.org.uk>
2017-08-25 11:04:41 +0100

    added project file, also corrected index.js in package.json

--- server.js
+++ server.js
@@ -50,6 +50,14 @@ app.get('/about', (req,res) => {
   });
 })
 
+app.get('/projects', (req,res) => {
+
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page!',
+
+  });
+})
+
 app.get('/bad', (req,res) => {
 
   res.send({
